bc76fb23d5047ae7ee9c91969db36546f9f258a3
thanhx99 <trungle9995@gmail.com>
2023-12-19 20:18:03 -0500

    link gundamScript.js to index.html

--- gundamScript.js
+++ gundamScript.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
-    
+    fetch('http://localhost:3000/gundams')
+        .then(resp =>{
+            if(resp.ok)
+            {
+                return resp.json()
+            }
+            throw new Error('NETWORK ERROR POINT #1')
+        })
+        .then(result =>{
+            result.forEach(function(elem){
+                console.log(elem)
+            })
+        })
 })
